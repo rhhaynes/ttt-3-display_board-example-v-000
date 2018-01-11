5b89dde13070363a11c7e6ad8aadb02289b1fbc5
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-11 14:51:36 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,12 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  row = "   |   |   "
+  sep = "-----------"
+  for i 1..5
+    if i.even?
+      puts sep
+    else
+      puts row
+    end
+  end
+end
